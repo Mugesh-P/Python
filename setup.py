6628ab29063ddc8cc127f4ccad486bb9d8222c4c
Mugesh P <164873002+Mugesh-P@users.noreply.github.com>
2025-11-12 12:21:11 +0530

    Rename project from 'reckitt_rnd_project' to 'mugesh_python'

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='reckitt_rnd_project',
+    name='mugesh_python',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'django>=4.0',
-        'djangorestframework',
-        # ... your other deps
-    ],
 )
